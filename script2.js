57c0c537de93ab749e7aedbae0e69c54a71f701d
rajkumar bawane <rajkumarbawane10@gmail.com>
2024-02-21 13:17:02 +0530

    introvert

---
+++ script2.js
@@ -0,0 +1,29 @@
+// introvert and extrovert
+
+let a = 10
+console.log( a)
+console.log(typeof a)
+// 10, -10, 19.9, -19.9, 0
+
+let b = 90.9
+console.log(typeof b)
+
+//program 2
+let k = true
+console.log(k)
+console.log(typeof k)
+
+//program 3
+let first_name = "rajkumar"
+console.log(first_name)
+console.log(typeof first_name)
+
+
+
+
+
+
+
+
+
+
