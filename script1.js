7756aac1eb297ced4c3644b33ad9d42115b24511
rajkumar bawane <rajkumarbawane10@gmail.com>
2024-02-21 13:09:10 +0530

    function

---
+++ script1.js
@@ -0,0 +1,87 @@
+//program 1
+
+console.log("Hello")
+let balance = 0
+console.log(balance)
+balance = 100
+console.log(balance)
+
+//program 2
+const pi = 3.14
+console.log(pi)
+
+
+//program 5
+
+let q = 8
+let r = 4
+
+console.log(q+r)
+console.log(q-r)
+console.log(q*r)
+console.log(q/r)
+console.log(q%r)
+
+let s = 10
+let t = 5
+
+console.log(s+t)
+console.log(s-t)
+console.log(s*t)
+console.log(s/t)
+console.log(s%t)
+
+
+function calculator(x,y){
+    console.log(x+y)
+    console.log(x-y)
+    console.log(x*y)
+    console.log(x/y)
+    console.log(x%y)
+}
+
+calculator(12,3)
+calculator(8,4)
+calculator(6,3)
+
+//program 5
+// function without parameter and without return type
+
+function addition(){
+    console.log(9+9)
+}
+addition()
+addition()
+addition()
+addition()
+
+// function with paeameter and with return type
+
+function addition(x,y){
+    console.log(x+y)
+
+}
+addition(12,3)
+addition(6,3)
+
+
+// function with parameter and with return type
+
+function addition(x,y){
+    return x+y
+}
+let q1 = addition(13,4)
+console.log(q1)
+
+
+
+
+
+
+
+
+
+
+
+
+
